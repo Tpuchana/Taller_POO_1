d43838c135b184f73738ea08c07d73d7cc0a6787
Tomás Puchana <109983088+Tpuchana@users.noreply.github.com>
2023-02-09 10:37:20 -0500

    taller

---
+++ src/taller1/Taller1.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package taller1;
+
+/**
+ *
+ * @author SALA
+ */
+public class Taller1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        // TODO code application logic here
+        
+        //Punto1
+       /* 1. declare 5 variables con la siguiente información: 
+ 
+     *Nombre completo 
+        *Edad  --> int
+        *Ciudad --> string
+        *Barrio --> string
+        *Universidad donde estudia --> string 
+        Luego  imprima  la  información  con  un  texto  similar  al  siguiente:  “Mi nombre es 
+        Wilken  Rodriguez  tengo  29  años  vivo  en  Bogotá  en  el  barrio  Las  ferias  y 
+        estudie en la Universidad Militar Nueva Granada” */
+       
+       String Nombre;
+       int Edad;
+       String Ciudad;
+       boolean MayorEdad;
+       float Estatura;
+       String Universidad_estudia;
+       
+    
+    }
+    
+}
