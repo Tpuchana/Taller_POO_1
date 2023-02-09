2254d53bebedfd07a59873f1fe1b1947733c9645
Tomás Puchana <109983088+Tpuchana@users.noreply.github.com>
2023-02-09 10:42:38 -0500

    1

--- Taller1/src/taller1/Taller1.java
+++ Taller1/src/taller1/Taller1.java
@@ -4,6 +4,10 @@
  */
 package taller1;
 
+import java.util.Scanner;
+
+
+
 /**
  *
  * @author SALA
@@ -14,7 +18,44 @@ public class Taller1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+ 
+       String Nombre;
+       int Edad;
+       String Ciudad;
+       boolean MayorEdad;
+       float Estatura;
+       String Universidad_estudia;
+       Scanner input = new Scanner(System.in);
+               
+       System.out.println("Ingrese su nombre: ");
+       Nombre = input.nextLine();
+       
+       System.out.println("Ingrese su edad: ");
+       Edad = input.nextInt();
+       
+       System.out.println("Ingrese su ciudad: ");
+       Ciudad = input.next();
+       
+       if (Edad >17){
+           MayorEdad = true;
+       }
+       
+       System.out.println("Ingrese su estatura: ");
+       Estatura = input.nextFloat();
+       
+       System.out.println("Ingrese la universidad donde estudia: ");
+       Universidad_estudia = input.next();
+       
+        System.out.println("Hola mi nombre es "+ Nombre + 
+                "\n tengo " + Edad + "años, vivo en " + Ciudad+
+                " y mido " + Estatura + "m ");
+       }
+       
+       
+       
+       
+    
     }
     
-}
+
+
