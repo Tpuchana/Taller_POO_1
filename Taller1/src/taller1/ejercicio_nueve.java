511a7ce1bf3da46c86bfd02849bcfb61e3f85bbd
Tomás Puchana <tpuchan33373@universidadean.edu.co>
2023-02-15 20:29:35 -0500

    2

---
+++ Taller1/src/taller1/ejercicio_nueve.java
@@ -0,0 +1,21 @@
+package taller1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class ejercicio_nueve {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
